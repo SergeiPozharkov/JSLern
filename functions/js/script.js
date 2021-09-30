5591d841f5ccd784537472fb6c152f8953f174b3
SergeiPozharkov <pozharkov.sergei@mail.ru>
2021-09-30 20:49:10 +0300

    added first JS function

---
+++ functions/js/script.js
@@ -0,0 +1,18 @@
+// function for finde min number
+// function min(a, b) {
+//     return a < b ? a : b;
+// }
+
+// let a = prompt('enter a', '');
+// let b = prompt('enter b', '');
+
+// alert('fewer number = ' + min(a, b));
+
+// function for exp number
+function pow(x, n) {
+    return x ** n;
+}
+
+let x = prompt('enter x', '');
+let n = prompt('enter n', '');
+alert('result = ' + pow(x, n));
