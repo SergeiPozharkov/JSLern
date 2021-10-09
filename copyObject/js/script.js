2c61babe04f1be6d632d9022bd2517cf0b55a6b9
SergeiPozharkov <pozharkov.sergei@mail.ru>
2021-10-09 15:26:05 +0300

    added copi object JS

---
+++ copyObject/js/script.js
@@ -0,0 +1,12 @@
+let testMsg = 'Hello!';
+let result = testMsg;
+let tenp1 = {};
+let tenp2 = {};
+
+console.log(result);
+
+console.log(testMsg == result);
+console.log(testMsg === result);
+
+console.log(tenp1 === tenp2);
+console.log(tenp1 == tenp2);
